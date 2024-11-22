db6588461b30551b25179a55f403c30f20749ce9
coderwell <90953900+coderwell824@users.noreply.github.com>
2024-11-22 11:21:23 +0800

    feat: 完善项目配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import { useEffect } from "react";
 const App = () => {
+  useEffect(() => {
+    console.log("111kjksjkdjs");
+  }, []);
+
   return (
     <div className="text-[#f00] font-medium w-3 h-4 bg-slate-800">
       App12222s d j k s j d k s j k d j s k
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import react from "@vitejs/plugin-react"
 import * as path from 'path';
 import electron from 'vite-plugin-electron';
 import electronRenderer from 'vite-plugin-electron/renderer';
 import polyfillExports from 'vite-plugin-electron/polyfill-exports';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import alias from "@rollup/plugin-alias";
-import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
 
 export default defineConfig({
   server: {
@@ -16,8 +13,7 @@ export default defineConfig({
   },
   mode: 'development', // development | production
   plugins: [
-    VueSetupExtend(),
-    vue(),
+    react(),
     electron({
       main: {
         entry: 'electron/index.ts',
@@ -29,12 +25,6 @@ export default defineConfig({
     }),
     electronRenderer(),
     polyfillExports(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
     alias()
   ],
   css: {
